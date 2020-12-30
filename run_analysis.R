a2da3c8bb256c3ef941738e87d77d78c6ac7db7c
Aidos-QNP <kanafin.dos@yandex.kz>
2020-12-30 17:35:37 +0600

    step1, merging data sets

--- run_analysis.R
+++ run_analysis.R
@@ -3,32 +3,45 @@
 
 # exploring data sets with read.table(), 
 
-pthTs_subj<-"test/subject_test.txt" # 2947x1 2-24 indexes
-pthTr_subj<-"train/subject_train.txt" # 7352x1 1-30 indexes
-pthTs_x<-"test/X_test.txt" # 2947x561 measurements
-pthTs_y<-"test/y_test.txt" # 2947x1 1-6 indexes
-pthTr_x<-"train/X_train.txt" # 7352x561 measurements
-pthTr_y<-"train/y_train.txt" # 7352x1 1-6 indexes
-pthFt<-"features.txt" # 561x2 names of the measurements (variables)
+pthTs_subj <- "test/subject_test.txt" # 2947x1 2-24 indexes
+pthTr_subj <- "train/subject_train.txt" # 7352x1 1-30 indexes
+pthTs_x <- "test/X_test.txt" # 2947x561 measurements
+pthTs_y <- "test/y_test.txt" # 2947x1 1-6 indexes
+pthTr_x <- "train/X_train.txt" # 7352x561 measurements
+pthTr_y <- "train/y_train.txt" # 7352x1 1-6 indexes
+pthFt <- "features.txt" # 561x2 names of the measurements (variables)
 
-pthBase<-"./data/UCI HAR Dataset/"
+pthBase <- "./data/UCI HAR Dataset/"
 
 dTsSbj <- read.table(paste0(pthBase, pthTs_subj))
 dTrSbj <- read.table(paste0(pthBase, pthTr_subj))
-dTsX<- read.table(paste0(pthBase, pthTs_x))
-dTsY<- read.table(paste0(pthBase, pthTs_y))
+dTsX <- read.table(paste0(pthBase, pthTs_x))
+dTsY <- read.table(paste0(pthBase, pthTs_y))
 dTrX <- read.table(paste0(pthBase, pthTr_x))
 dTrY <- read.table(paste0(pthBase, pthTr_y))
-dFt<-  read.table(paste0(pthBase, pthFt))
+dFt <-  read.table(paste0(pthBase, pthFt))
 # after reading Read me file and analyzing data sets with 
 # as_tibble (tibble library), # dim() and summary() I wrote
 # result as comments in 6-11 lines: dimension and form of the data
 
-# step 1, merging data sets 
+# I decide to organize variables (columns) so: "subject", 
+# "activity" and 561 features from features.txt
 
-dim(dFt)
-head(pthTs_y)
-summary(dTsY)
+# step 1, merging data sets:
+# first, all train columns
+dwTr<-cbind(dTrSbj, dTrY, dTrX)
+# then all test columns
+dwTs<-cbind(dTsSbj, dTsY, dTsX)
+# then merge this two data sets by rows
+dbig<-rbind(dwTr, dwTs)
+
+
+
+dbig[8000, 1:5]
+
+dim(dwTr)
+head(dwTr, 3)
+summary(dwTr)
 install.packages("tibble")
 library(tibble)
 d1<-as_tibble(d1)
