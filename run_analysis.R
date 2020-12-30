241a26c66cd650d3880aa4d25382aee9a419301e
Aidos-QNP <aidosqanapia@gmail.com>
2020-12-30 23:12:45 +0600

    Some test2

--- run_analysis.R
+++ run_analysis.R
@@ -61,11 +61,11 @@ nmAct[2]
 summary(dmean[,2])
 
 dmean<-as_tibble(dmean)
-
+vfdgf
 dmean
 dbig[4, 1:10]
 as_tibble(dbig)
-
+gfgdf
 tt<-grep("mean", names(dbig), value=TRUE, ignore.case = TRUE) 
 length(tt)
 tt
