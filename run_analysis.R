4665a322485cfbab4dc7ac5c4ca2eda2e059ec72
Aidos-QNP <aidosqanapia@gmail.com>
2021-01-01 14:18:26 +0600

    cleaning: comments, white spaces, etc.

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,12 @@
-#fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
-#download.file(fileUrl,destfile="./data/fgdp.csv")
+# sorry for my English!
+# My Github: https://github.com/Aidos-QNP/course3-assignment
 
 # exploring data sets with read.table(), 
-
 pthTs_subj <- "test/subject_test.txt" # 2947x1 2-24 indexes
 pthTr_subj <- "train/subject_train.txt" # 7352x1 1-30 indexes
-pthTs_x <- "test/X_test.txt" # 2947x561 measurements
+pthTs_x <- "test/X_test.txt" # 2947x561 values (measurements?)
 pthTs_y <- "test/y_test.txt" # 2947x1 1-6 indexes
-pthTr_x <- "train/X_train.txt" # 7352x561 measurements
+pthTr_x <- "train/X_train.txt" # 7352x561 values (measurements?)
 pthTr_y <- "train/y_train.txt" # 7352x1 1-6 indexes
 pthFt <- "features.txt" # 561x2 names of the measurements (variables)
 
@@ -21,8 +20,8 @@ dTrX <- read.table(paste0(pthBase, pthTr_x))
 dTrY <- read.table(paste0(pthBase, pthTr_y))
 dFt <-  read.table(paste0(pthBase, pthFt))
 # after reading Read me file and analyzing data sets with 
-# as_tibble (tibble library), # dim() and summary() I wrote
-# result as comments in 6-11 lines: dimension and form of the data
+# as_tibble (tibble library), dim() and summary() I wrote
+# result as comments in 5-11 lines: dimension and form of the data
 
 # I decide to organize variables (columns) so: "subject", 
 # "activity" and 561 features from features.txt
@@ -38,17 +37,11 @@ dbig<-rbind(dwTr, dwTs)
 # delete some big objects for optimize computer memory:
 rm(dTsSbj, dTrSbj, dTsX, dTsY, dTrX, dTrY, dwTr, dwTs)
 
-
-# ERROR: to assign variables names first we need modify feature name set
-# by adding two new variable names
-# ERROR: names(dbig) <- c(c("subject", "activity"), dFt[,2])
-
 #step 2, subset mean and standart deviation columns:
 maskMean<-grepl("-mean|-std", dFt[,2])
 # we need to leave first two column, so lets include to mask:
 maskMean <- c(c(TRUE, TRUE), maskMean)
 dmean<-dbig[,maskMean]
-
 
 # step 3, descriptive activity names:
 nmAct <- c("Walking", "Walking Upstairs", 
@@ -76,16 +69,11 @@ nm<-gsub("\\(\\)", "", nm)
 
 names(dmean)<-nm
 
-# library(tibble)
-# dmean<- as_tibble(dmean)
-
-
 # step 5
 library(reshape2)
 dml<-melt(dmean, id=c("subject", "activity"), 
           measure.vars = names(dmean[, 3:81]))
 names(dml)[3]<-"measurement"
-
 
 library(dplyr)
 dty<- dml %>% group_by(subject, activity, measurement) %>%
@@ -94,69 +82,8 @@ dty<- dml %>% group_by(subject, activity, measurement) %>%
     arrange(subject, .by_group = TRUE)
 names(dty)[4]<-"mean"
 
+head(dty)
+
 # code for writing to file and reading:
 # write.table(dty, "tidy_data_set.txt")
 # dd<-read.table("tidy_data_set.txt")
-
-
-
-dim(dd)
-head(dd)
-head(dty)
-dim(dty)
-dd<-filter(dty, (subject==1 & activity=="Walking"
-                 & measurement == "timeBodyAccelerationMeanX"))
-#> mean(dd$value)
-#[1] 0.2773308
-
-
-dim(dd)
-head(dd)
-tail(dd)
-mean(dd$value)
-
-?filter
-head(dty)
-
-head(dty3)
-
-
-?mutate
-
-
-
-
-dty4[500:518, ]
-dim(dty3)
-#dty3<-select(dty3, -value)
-
-dim(dty4)
-
-
-
-head(dty4, 8)
-
-dty4[75:85, ]
-?arrange
-?group_by
-library(plyr)
-
-dty2<-ddply(dty, .(measurement), summarize, sum=mean(count))
-
-warnings()
-head(dty)
-
-dty<-sapply(mean, dmean, list(dmean[,3:81]))
-
-library(dplyr)
-dmean<-group_by(dmean, subject, activity)
-
-
-library(reshape2)
-?dcast
-dty<- mutate(dmean, d)
-dim(dmean)
-unique(dmean)
-dmean
-?with
-sum(dmean[, 1]==30)
